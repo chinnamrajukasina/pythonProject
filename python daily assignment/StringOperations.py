20593c0d0f2158fd9017bdf237d7afb56bd347f5
chinnamrajukasina <chinnamrajukaina@gmail.com>
2023-11-14 16:56:19 +0530

    updated code improvements new files

---
+++ python daily assignment/StringOperations.py
@@ -0,0 +1,16 @@
+# Create a string
+original_string = "Hello, "
+
+# Concatenate strings
+concatenated_string = original_string + "World!"
+
+# Print the concatenated string
+print("Concatenated String:", concatenated_string)
+
+# Accessing Substring
+start_index = 7
+end_index = 12
+substring = concatenated_string[start_index:end_index]
+
+# Print the substring
+print("Substring:", substring)
